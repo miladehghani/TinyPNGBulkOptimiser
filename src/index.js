7613e6aabbda298c9010802a94d098d017f5e017
Milad <Milad.1372.g@gmail.com>
2020-12-04 11:42:21 +0330

    update readme, bugfix

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
+const { key } = require("tinify");
 const Optimise = require("./imageOptimiser");
 
-const vars = process.argv.splice(2);
+const vars = process.argv.slice(2);
 let path = "";
 let keys = [];
-
 for (let i = 0; i < vars.length; i++) {
   switch (vars[i]) {
     case "--path":
       path = vars[i + 1];
       break;
     case "--keys":
-      keys = vars.splice(i + 1);
+      keys = vars.slice(i + 1);
   }
 }
-if (!path) console.log("Please set the --path");
-if (keys.length)
-  console.log("Please set at least one Tinify API KEY using alias --key");
 
-Optimise(keys, path);
+if (!path) console.log("Please set the --path");
+else if (keys.length == 0)
+  console.log("Please set at least one Tinify API KEY using alias --keys");
+else Optimise(keys, path);
